972752b705bcab220955cc6b46e368dbb08fd026
Jose Neilson Mc <neilsonvlcdn@gmail.com>
2024-02-20 19:55:28 -0300

    #

--- my-Hello-World/src/Components/InformationContainer.jsx
+++ my-Hello-World/src/Components/InformationContainer.jsx
@@ -11,6 +11,9 @@ import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from "react-icons/ai";
         <div>
           <h3>Telefone</h3>
           <p>(48)9 8839-0463</p>
+          <ul>
+          <li><a href="https://api.whatsapp.com/send?phone=5549998130442&text=Texto%20aqui" target="_blank">whatsapp</a></li>
+          </ul>
         </div>
       </div>
       <div className="info-card">
@@ -18,6 +21,9 @@ import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from "react-icons/ai";
         <div>
           <h3>E-mail</h3>
           <p>neilsonvlcdn@gmail.com</p>
+          <ul>
+          <li><a href="mailto:neilsonvlcdn@gmail.com">Enviar um e-mail</a></li>
+          </ul>
         </div>
       </div>
       <div className="info-card">
--- my-Hello-World/src/Components/SocialNetworkContainer.jsx
+++ my-Hello-World/src/Components/SocialNetworkContainer.jsx
@@ -4,9 +4,9 @@ import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
 import "../styles/Components/socialnetworkcontainer.sass";
 
 const socialNetworks = [
-  { name: "linkedin", icon: <FaLinkedinIn /> },
-  { name: "github", icon: <FaGithub /> },
-  { name: "instagram", icon: <FaInstagram /> },
+  { name: "linkedin", icon: <FaLinkedinIn />, link:<a href="https://www.linkedin.com/in/neisonsantos/#:~:text=www.linkedin.com/in/neisonsantos" target="_blank">Linkedin</a>},
+  { name: "github", icon: <FaGithub />,link:<a href="https://github.com/JoseNeilsonMc" target="_blank"></a> },
+ // { name: "instagram", icon: <FaInstagram /> },
 ];
 
  const SocialNetworkContainer = () => {
@@ -14,7 +14,7 @@ const socialNetworks = [
     <div>
          <section id="social-networks">
       {socialNetworks.map((network) => (
-        <a href="#" 
+        <a href={network.link} 
            className="social-btn" 
            id={network.name} 
            key={network.name}>
